e3bc5b027777d261a8c213113b49b38f498e8eec
dam224 <jsainzv01@educantabria.es>
2022-01-26 14:02:04 +0100

    2 files modified

--- src/components/Perfil.jsx
+++ src/components/Perfil.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
 
-export function Perfil() {
-  return (
-    <div className="main-site">
-      <h1>Página de perfil</h1>
-    </div>
-  );
+
+class Perfil extends React.Component {
+  constructor(props) {
+    super(props);
+   
+  }
+    render () {
+            return (
+              <div className="main-site">
+                <h1>Página de perfil</h1>
+              </div>
+            );
+         }
 }
+export default Perfil;
---
+++ src/data/DatosPerfil.js
@@ -0,0 +1,11 @@
+export const usuarios =[
+  {
+    nombreusuario: 'Jose',
+    password:'1234',
+    email:'jose@jose.es',
+
+  }
+
+
+
+]
